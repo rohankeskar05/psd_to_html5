f59e58c2e3893e3e22558cdeeac530b6bca5e662
rohankeskar05 <35923489+rohankeskar05@users.noreply.github.com>
2019-07-16 12:25:32 +0530

    added animation part

--- js/main.js
+++ js/main.js
@@ -1,14 +1,25 @@
 $(document).ready(function() {
 
-    'use strict';
+    new WOW().init();
 
-    var typed = $(".typed");
+    $('.js-wp-2').waypoint(function(direction) {
 
-    $(function() {
-        typed.typed({
-            strings: ["Rohan Keskar.", "Designer.", "Developer.", "Trekker.", "Photographer"],
-            typeSpeed: 100,
-            loop: true,
-        });
+        $('.js-wp-2').addClass('animated slideInUp');
+
+    }, {
+        offset: '70%'
     });
+
+    $('.js-wp-3').waypoint(function(direction) {
+
+        $('.js-wp-3').addClass('animated fadeIn');
+
+    }, {
+        offset: '70%'
+    });
+
+
+    $(".iphone-btn").delay(1500).animate({ bottom: "+=-3" }, 200);
+    $(".iphone-btn").delay(300).animate({ top: "+=-3" }, 100);
+
 });
